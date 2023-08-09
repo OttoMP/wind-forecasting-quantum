ecf85d28f935995295f27817377d49b4ac17c1ab
OttoMP <otto28mai@gmail.com>
2023-08-09 09:57:11 -0300

    saving mid progress

---
+++ main.py
@@ -0,0 +1,138 @@
+import sys
+import os
+import tensorflow as tf
+import pennylane as qml
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from quantum_neural_network import qnode_entangling
+from statistics_1 import quantitative_analysis
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def plot_history(history):
+    plt.figure(figsize=(14,5), dpi=320, facecolor='w', edgecolor='k')
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.plot(history.history['loss'], label="Loss/Epoch")
+    plt.plot(history.history['val_loss'], label="Val Loss/Epoch")
+    plt.legend()
+
+    path = os.path.abspath(os.path.join(os.getcwd(), 'plots'))
+    filename = "experiment-1.svg"
+    plt.savefig(os.path.join(path,filename))
+
+
+def carregar_tabela(path):
+    dataframe=pd.read_csv(path)
+    dataset = dataframe.values
+    X_train_all=dataframe[['Total amount of precipitation','Vapor_Pressure','Relative_Humudity','Amount of cloudiness','Actual_Pressure']]
+    y_train_all=dataframe[['Temperature']]
+
+    return X_train_all, y_train_all.values
+
+
+def main():
+
+    ########################
+    ### Importando dados ###
+    ########################
+
+    filename = sys.argv[1]
+
+    X_all,y_all = carregar_tabela(filename)
+    #y_train_all = y_train_all.reshape(-1,1)
+
+    n_features = X_all.shape[1]
+    n_instances = X_all.shape[0]
+    print(f"There are {n_features} features and {n_instances} instâncias")
+    print(X_all.head())
+    print(y_all[:5])
+
+    ####################
+    ### Scaling Data ###
+    ####################
+    
+    scaler_x = StandardScaler()
+    X_all_scaled = scaler_x.fit_transform(X_all)
+    #scaler_y = StandardScaler()
+    #y_all_scaled = scaler_y.fit_transform(y_all)
+
+    #####################################
+    ### Splitting Train and Test sets ###
+    #####################################
+    train_ratio = 0.75
+    validation_ratio = 0.15
+    test_ratio = 0.10
+    # train is now 75% of the entire data set
+    X_train, X_test, y_train, y_test = train_test_split(X_all_scaled, y_all, test_size=1 - train_ratio)
+    # test is now 10% of the initial data set
+    # validation is now 15% of the initial data set
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio)) 
+    
+    #X_train = tf.cast(X_train, dtype=tf.float64)
+    #y_train = tf.cast(y_train, dtype=tf.float64)
+    print("Len(Train):",len(X_train))
+    print("Len(Val):"  ,len(X_val))
+    print("Len(Test):" ,len(X_test))
+
+
+    n_qubits = n_features
+    y_test_pred = []
+    for n_layers in range(1,2):
+        ##########################################
+        ### Creating Neural Network with Keras ###
+        ##########################################
+        print(f"Serão necessários {n_qubits} qubits")
+        n_layers = 1
+        weight_shapes = {"weights": (n_layers,n_qubits,3)}
+
+        q_layer = qml.qnn.KerasLayer(qnode_entangling, weight_shapes, output_dim=n_qubits)
+        Activation=tf.keras.layers.Activation(tf.keras.activations.linear)
+        output_layer = tf.keras.layers.Dense(1,kernel_initializer='normal')
+
+        opt = tf.keras.optimizers.Adam(learning_rate=0.1)
+
+        model = tf.keras.models.Sequential([q_layer,Activation, output_layer])
+        model.compile(opt, loss="mse")
+
+        input_shape = (n_qubits,)
+
+        model.build(input_shape)
+        print(model.summary())
+
+        ######################
+        ### Training Model ###
+        ######################
+        
+        es=EarlyStopping(monitor='val_loss', min_delta=0, patience=6, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
+        re=ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, mode='min', min_lr=0.00001)
+        history_model = model.fit(X_train, y_train
+                                , epochs=30, batch_size=64
+                                , callbacks=[es, re]
+                                , verbose=1
+                                , validation_data=(X_val, y_val))
+
+        #################
+        ### Loss Plot ###
+        #################
+        plot_history(history_model)
+        y_test_pred.appen(model.predict(X_test,verbose=1))
+        #y_test_pred_normal = scaler_y.inverse_transform(y_test_pred)
+
+    #####################
+    ### Data Analysis ###
+    #####################
+    print("Len y_pred", len(y_test_pred))
+    print(y_test_pred)
+    #erros_pd = quantitative_analysis(y_test, y_test_pred)
+    #print(erros_pd)
+
+    #mean_predictions, mean_error_normal, mean_error_left_normal, mean_error_right_normal = get_mean_left_right_error_interval(
+    #model, scaler_x, X_val, y_val, y_test, y_test_pred)
+
+
+if __name__ == "__main__":
+    main()
---
+++ statistics_1.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+from scipy import stats  
+from sklearn.metrics import mean_squared_error, mean_absolute_error,r2_score
+from tensorflow.keras import backend
+
+def factor_of_2(y_true, y_pred):
+    min_ = 0.5
+    max_ = 2.0
+
+    tensor_true = tf.constant(y_true)
+    tensor_true = tf.cast(tensor_true, tf.float32)
+    tensor_pred = tf.constant(y_pred)
+    tensor_pred = tf.cast(tensor_pred, tf.float32)
+
+    division = tf.divide(tensor_pred, tensor_true)
+
+    greater_min = tf.greater_equal(division, min_)
+    less_max = tf.less_equal(division, max_)
+
+    res = tf.equal(greater_min, less_max)
+    res = tf.cast(res, tf.float32)
+
+    return backend.get_value(tf.reduce_mean(res))
+
+def allmetrics(original,predito):
+    r_value = 0
+    slope, intercept, r_value, p_value, std_err = stats.linregress(original, predito)
+    mse = mean_squared_error(original, predito)
+    mae = mean_absolute_error(original, predito)
+    rr = r2_score(original,predito)
+    pea = stats.pearsonr(original, predito)
+    fat = factor_of_2(original,predito)
+    nmse = mse/stats.tvar(original)
+    rmse = np.sqrt(mse)
+    nrmse = rmse/stats.tstd(original)
+    return mae,mse,nmse,r_value,rr,fat,rmse,nrmse
+
+def get_error_interval(model, X_val, y_val, y_test_pred, p_value):
+    y_val_pred = model.predict(X_val)
+    y_val_error = np.abs(y_val - y_val_pred)
+    
+    error_quantile=np.ndarray((1,y_val.shape[1]))
+    for i in range(y_val.shape[1]):
+        error_quantile[0,i] = np.quantile(y_val_error[:,i], q=p_value, interpolation='higher')
+        
+    y_test_interval_pred_left=np.ndarray(y_test_pred.shape)
+    y_test_interval_pred_right=np.ndarray(y_test_pred.shape)
+    
+    for i in range(y_test_pred.shape[1]):
+        y_test_interval_pred_left[:,i] = y_test_pred[:,i] - error_quantile[0,i]
+        y_test_interval_pred_right[:,i] = y_test_pred[:,i] + error_quantile[0,i]
+    
+    return error_quantile, y_test_interval_pred_left, y_test_interval_pred_right
+
+def get_mean_left_right_error_interval(model, scaler, X_val, y_val, y_test, y_test_pred):
+    error, error_left, error_right = get_error_interval(model, X_val, y_val, y_test_pred, 0.95)
+    
+    #error_left_normal = scaler.inverse_transform(error_left)
+    #error_right_normal = scaler.inverse_transform(error_right)
+    error_left_normal  = error_left
+    error_right_normal = error_right
+
+    mean_error_normal      = np.ndarray((1,y_test.shape[1]))
+    mean_error_left_normal = np.ndarray((1,y_test.shape[1]))
+    mean_error_right_normal= np.ndarray((1,y_test.shape[1]))
+    mean_predictions       = np.ndarray((1,y_test_pred.shape[1]))
+
+    for i in range(y_test.shape[1]):
+        mean_error_left_normal[0,i]  = np.mean(error_left_normal[:,i])
+        mean_error_right_normal[0,i] = np.mean(error_right_normal[:,i])
+        mean_predictions[0,i]        = np.mean(y_test_pred[:,i])
+
+    mean_error_normal = (mean_error_right_normal - mean_error_left_normal)/2
+    
+    return mean_predictions, mean_error_normal, mean_error_left_normal, mean_error_right_normal
+
+def quantitative_analysis(y_test, y_test_pred):
+    valores = []
+    for i in range(y_test.shape[1]):
+        mae,mse,nmse,r_value,rr,fat,rmse,nrmse = allmetrics(y_test[:,i],y_test_pred[:,i])
+        #valores.append([str(i+1)+" depth",mae,mse,nmse,rmse,nrmse,r_value,rr,fat,mean_error_normal[0,i],mean_error_left_normal[0,i],mean_predictions[0,i],mean_error_right_normal[0,i]])
+        valores.append([str(i+1)+" depth",mae,mse,nmse,rmse,nrmse,r_value,rr,fat])
+        print("MAE:",mae)
+        print("MSE:",mse)
+        print("NMSE:",nmse)
+        print("RMSE:",rmse)
+        print("NRMSE:",nrmse)
+        print("R:",r_value)
+        print("R²:",rr)
+        print("Fator de 2:",fat)
+
+    erros = pd.DataFrame(valores)
+    erros.columns = ['Index','MAE','MSE','NMSE','RMSE','NRMSE','R','R²','Fator de 2', 'error interval (+/-)', 'left limit', 'mean', 'right limit']
+    erros = erros.set_index('Index')
+    erros.loc['Média'] = erros.mean()
+    return erros
